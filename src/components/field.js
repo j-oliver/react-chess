c47e780af413bbd1f474e94826f0843c6f287769
Jan <jan.rollmann94@googlemail.com>
2018-03-02 19:00:33 +0100

    Rework getPiece of the field component to create components with less code

--- src/components/field.js
+++ src/components/field.js
@@ -6,42 +6,36 @@ import Rook from './rook';
 import Bishop from './bishop';
 import Knight from './knight';
 import Pawn from './pawn';
+import EmptyField from './emptyfield';
 
+const pieces = {
+  k: King,
+  q: Queen,
+  r: Rook,
+  b: Bishop,
+  n: Knight,
+  p: Pawn,
+}
 
 class Field extends Component {
   getPiece() {
-    switch(this.props.name.toLowerCase()) {
-      case 'k':
-        return <King name={this.props.name}
-                     onPieceClick={this.props.onPieceClick}
-                     position={this.props.position}
-                     getField={this.props.getField}/>;
-      case 'q':
-        return <Queen name={this.props.name}
-                     onPieceClick={this.props.onPieceClick}
-                     position={this.props.position}
-                     getField={this.props.getField}/>;
-      case 'r':
-        return <Rook name={this.props.name}
-                     onPieceClick={this.props.onPieceClick}
-                     position={this.props.position}
-                     getField={this.props.getField}/>;
-      case 'b':
-        return <Bishop name={this.props.name}
-                     onPieceClick={this.props.onPieceClick}
-                     position={this.props.position}
-                     getField={this.props.getField}/>;
-      case 'n':
-        return <Knight name={this.props.name}
-                     onPieceClick={this.props.onPieceClick}
-                     position={this.props.position}
-                     getField={this.props.getField}/>;
-      case 'p':
-        return <Pawn name={this.props.name}
-                     onPieceClick={this.props.onPieceClick}
-                     position={this.props.position}
-                     getField={this.props.getField}/>;
-      default: return undefined;
+    const identifier = this.props.name.toLowerCase();
+
+    if(identifier in pieces) {
+      return React.createElement(pieces[identifier], {
+        name: this.props.name,
+        color: this.props.color,
+        onPieceClick: this.props.onPieceClick,
+        position: this.props.position,
+        getField: this.props.getField,
+        select: this.props.select,
+      });
+    } else {
+      return <EmptyField
+        name={this.props.name}
+        position={this.props.position}
+        select={this.props.select}
+      />;
     }
   }
 
